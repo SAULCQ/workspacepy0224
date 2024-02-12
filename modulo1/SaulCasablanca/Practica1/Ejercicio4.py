ad59819aaeeec61db44ae7f8f5e5b8e71f8990a1
SAULCQ <158852384+SAULCQ@users.noreply.github.com>
2024-02-12 00:46:12 +0000

    Ejercicio4

--- modulo1/SaulCasablanca/Practica1/Ejercicio4.py
+++ modulo1/SaulCasablanca/Practica1/Ejercicio4.py
@@ -1,1 +1,15 @@
-print("hola mundo")
+# Ejercicio 4
+Edad=input("Ustes es mayor de edad? (Si/No): ")
+Ruc=input("Usted cuenta con RUC? (Si/No): ")
+Nombre=input("Usted cuenta con un nombre comercial? (Si/No): ")
+if Edad.upper()=="SI":
+    if Ruc.upper()=="SI":
+        if Nombre.upper()=="SI":
+            print("Ustes es apto para abrir un comercio")
+        
+        else:
+            print("Ustes NO es apto para abrir un negocio")
+    else:
+        print("Ustes NO es apto para abrir un negocio")    
+else:
+    print("Ustes NO es apto para abrir un negocio")
